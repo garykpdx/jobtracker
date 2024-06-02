1b2e83e8c05ffe772ed33a9b797ceb2dad9cd4d5
Gary Krug <garykpdx145@gmail.com>
2024-06-02 13:39:36 -0700

    first addition

---
+++ jobtracker/jobtracker/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for jobtracker project.
+
+The `urlpatterns` list routes URLs to templates. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function templates
+    1. Add an import:  from my_app import templates
+    2. Add a URL to urlpatterns:  path('', templates.home, name='home')
+Class-based templates
+    1. Add an import:  from other_app.templates import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homepage),
+    path('jobapps/', include('jobapps.urls')),
+    path('users/', include('users.urls')),
+]
