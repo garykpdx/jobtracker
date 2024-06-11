fd8622582c6f3f481f6e2fcba8a2bc7a06856103
Gary Krug <garykpdx145@gmail.com>
2024-06-11 14:05:49 -0700

    adding login page with authentication

--- jobtracker/users/views.py
+++ jobtracker/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+)
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django import forms
 
 
@@ -18,8 +22,19 @@ def register_view(request):
     if request.method == "POST":
         form = RegisterUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("jobapps")
     else:
         form = RegisterUserForm()
     return render(request, "users/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("jobapps")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
