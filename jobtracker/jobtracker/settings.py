b6ea5c65ced1f3c4cb24bcba89b6deb86408aeac
Gary Krug <garykpdx145@gmail.com>
2024-06-12 15:02:45 -0700

    remove os.path from settings, rearrange settings

--- jobtracker/jobtracker/settings.py
+++ jobtracker/jobtracker/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+
+MEDIA_ROOT = BASE_DIR / 'images'
 
 STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
 ]
-
-MEDIA_URL = 'images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
