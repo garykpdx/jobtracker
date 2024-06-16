eb871d6838e76f504694661ba08c01db5c1ec580
Gary Krug <garykpdx145@gmail.com>
2024-06-16 13:38:40 -0700

    remove slug from app, replace with ID (job_id)

--- jobtracker/jobapps/urls.py
+++ jobtracker/jobapps/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.jobapp_list, name="jobapps"),
     path('new-job/', views.new_jobapp, name="new-job"),
-    path("<slug:slug>", views.jobapp_page, name="jobapp"),
+    path("<int:job_id>", views.jobapp_page, name="jobapp"),
 ]
