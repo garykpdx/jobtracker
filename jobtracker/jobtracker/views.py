3a34cb00455ffa9857f6977da2d61d8d75f0e077
Gary Krug <garykpdx145@gmail.com>
2024-06-16 12:08:52 -0700

    view total jobs per day

--- jobtracker/jobtracker/views.py
+++ jobtracker/jobtracker/views.py
@@ -6,6 +6,6 @@ from jobapps.models import JobApp
 def homepage(request):
     # return HttpResponse("Hello world")
     jobcount = JobApp.objects.count()
-    count_by_date = JobApp.objects.annotate(Count("applied_dt"))
+    count_by_date = JobApp.objects.filter(user=request.user).values("applied_dt").annotate(date_count=Count("applied_dt"))
 
     return render(request, 'home.html', {'jobcount':jobcount, 'daily_counts': count_by_date})
