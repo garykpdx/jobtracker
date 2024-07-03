9d1620a7a0ccb755d12b25249fd2670672ca2932
Gary Krug <garykpdx145@gmail.com>
2024-07-03 15:58:43 -0700

    only show statistics for last 30 days

--- jobtracker/jobtracker/views.py
+++ jobtracker/jobtracker/views.py
@@ -1,14 +1,19 @@
+from datetime import date, timedelta
+
 from django.db.models import Count
-from django.http import HttpResponse
 from django.shortcuts import render
 from jobapps.models import JobApp
 
+
 def homepage(request):
-    # return HttpResponse("Hello world")
-    jobcount = JobApp.objects.filter(user=request.user).count()
+    today = date.today()
+    start_date = today - timedelta(days=30)
+    jobcount = JobApp.objects.filter(applied_dt__range=(start_date, today)).filter(user=request.user).count()
+
     count_by_date = ((JobApp.objects.filter(user=request.user)
-                     .values("applied_dt")
-                     .annotate(date_count=Count("applied_dt")))
+                      .filter(applied_dt__range=(start_date, today))
+                      .values("applied_dt")
+                      .annotate(date_count=Count("applied_dt")))
                      .order_by("-applied_dt"))
 
-    return render(request, 'home.html', {'jobcount':jobcount, 'daily_counts': count_by_date})
+    return render(request, 'home.html', {'jobcount': jobcount, 'daily_counts': count_by_date})
