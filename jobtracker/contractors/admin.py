15dcda87c29b0b9686b8e9012048555e2446e8c1
Gary Krug <garykpdx145@gmail.com>
2024-08-10 18:21:10 -0400

    add fields to admin

--- jobtracker/contractors/admin.py
+++ jobtracker/contractors/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Contractor
+
+
 # Register your models here.
-admin.site.register(Contractor)
+@admin.register(Contractor)
+class ContractorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'phone', 'email')
+    ordering = ('-created_dt',)
+    search_fields = ('name', 'company', 'phone', 'email')
--- jobtracker/jobapps/admin.py
+++ jobtracker/jobapps/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import JobApp
 
+
 # Register your models here.
-admin.site.register(JobApp)
+@admin.register(JobApp)
+class JobAppAdmin(admin.ModelAdmin):
+    list_display = ('title', 'applied_dt', 'job_status', 'job_number', 'company', 'location_type', 'city', 'state')
+    list_filter = ('applied_dt', 'job_status', 'location_type', 'job_source', 'user')
+    ordering = ('-applied_dt',)
+    search_fields = ('title', 'company', 'description', 'job_number', 'city', 'state')
