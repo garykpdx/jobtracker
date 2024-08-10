5a0df23b2cc7aff644e4bed1d76202a84640d740
Gary Krug <garykpdx145@gmail.com>
2024-08-10 12:59:52 -0400

    handle count exceptions on stats page

--- jobtracker/jobtracker/views.py
+++ jobtracker/jobtracker/views.py
@@ -8,12 +8,17 @@ from jobapps.models import JobApp
 def homepage(request):
     today = date.today()
     start_date = today - timedelta(days=30)
-    jobcount = JobApp.objects.filter(applied_dt__range=(start_date, today)).filter(user=request.user).count()
+    try:
+        jobcount = JobApp.objects.filter(applied_dt__range=(start_date, today)).filter(user=request.user).count()
 
-    count_by_date = ((JobApp.objects.filter(user=request.user)
-                      .filter(applied_dt__range=(start_date, today))
-                      .values("applied_dt")
-                      .annotate(date_count=Count("applied_dt")))
-                     .order_by("-applied_dt"))
+        count_by_date = ((JobApp.objects.filter(user=request.user)
+                          .filter(applied_dt__range=(start_date, today))
+                          .values("applied_dt")
+                          .annotate(date_count=Count("applied_dt")))
+                         .order_by("-applied_dt"))
+    except Exception as e:
+        print(e)
+        jobcount = 0
+        count_by_date = []
 
     return render(request, 'home.html', {'jobcount': jobcount, 'daily_counts': count_by_date})
