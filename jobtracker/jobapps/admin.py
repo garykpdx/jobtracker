d1072af64ecb3453757ca5e37e937426f329d1d4
Gary Krug <garykpdx145@gmail.com>
2024-08-11 11:08:57 -0400

    add available fields to admin page

--- jobtracker/jobapps/admin.py
+++ jobtracker/jobapps/admin.py
@@ -5,6 +5,8 @@ from .models import JobApp
 # Register your models here.
 @admin.register(JobApp)
 class JobAppAdmin(admin.ModelAdmin):
+    fields = ('title', 'job_status', 'job_number', 'description', 'company', 'payrate',
+              'location_type', 'city', 'state', 'contractor_name', 'job_source', 'job_url', 'user')
     list_display = ('title', 'applied_dt', 'job_status', 'job_number', 'company', 'location_type', 'city', 'state')
     list_filter = ('applied_dt', 'job_status', 'location_type', 'job_source', 'user')
     ordering = ('-applied_dt',)
