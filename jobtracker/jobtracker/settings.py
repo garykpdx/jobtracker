84649148531dd653694ff1ae511cc4f0be2ded53
Gary Krug <garykpdx145@gmail.com>
2026-09-09 23:46:00 -0500

    make DIRS full path

--- jobtracker/jobtracker/settings.py
+++ jobtracker/jobtracker/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'jobtracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
