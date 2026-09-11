88599d06477c097cb203c78c0b22e9e465ed2fc4
Gary Krug <garykpdx145@gmail.com>
2026-09-11 18:26:31 -0500

    modify settings for production

--- jobtracker/jobtracker/settings.py
+++ jobtracker/jobtracker/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import sqlite3
 from pathlib import Path
 
 import django.middleware.locale
-import sqlite3
-sqlite3.enable_callback_tracebacks(True)
+import environ
 
+sqlite3.enable_callback_tracebacks(True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-34ow^_=h2-9n$t-2o$x_g6rdytp+%0r-@c0o%&u8e(b91fkls_'
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise Exception("SECRET_KEY environment variable is not set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
