0cbd5edec8c668e5abd8529451d04bf58db630b5
Gary Krug <garykpdx145@gmail.com>
2026-09-15 18:45:03 -0500

    ensure logo appears on screen

--- jobtracker/jobtracker/urls.py
+++ jobtracker/jobtracker/urls.py
@@ -27,7 +27,7 @@ from django.urls import (
 from . import views
 
 urlpatterns = [
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    # re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('', views.homepage),
